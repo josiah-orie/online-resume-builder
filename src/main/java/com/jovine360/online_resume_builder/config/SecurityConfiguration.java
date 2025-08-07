22ee254ef67a17a7443839503d46e77ca101a9bf
Josiah Orie <josiahonuorie@gmail.com>
2025-08-07 16:15:16 +0100

    feat(src/main/java/com/jovine360/online_resume_builder/config): add new functionality
    Changes:
    - src/main/java/com/jovine360/online_resume_builder/config/SecurityConfiguration.java (+/-)
    
    # Affected areas:
    # - Backend/Core functionality

--- src/main/java/com/jovine360/online_resume_builder/config/SecurityConfiguration.java
+++ src/main/java/com/jovine360/online_resume_builder/config/SecurityConfiguration.java
@@ -4,7 +4,6 @@ package com.jovine360.online_resume_builder.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -13,7 +12,6 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.util.matcher.AndRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
